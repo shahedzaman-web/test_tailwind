aeea094bed087482c1c177e9e0fdba250136d08d
Md. Shaheduzzaman <77052145+shahedzaman-web@users.noreply.github.com>
2024-08-09 00:35:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <Footer />
+    </>
+  );
+}
